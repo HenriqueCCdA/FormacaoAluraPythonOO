8b5773ce6a01caf30074f3f6621e5c91561db122
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-27 18:55:50 -0300

    Inicio do Curso 1 (27/12/2023)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Formacao Alura Python OO
+
+1. [Curso de Python: crie a sua primeira aplicação](https://cursos.alura.com.br/course/python-crie-sua-primeira-aplicacao): Inicio 27/12/2023 - Fim 00/00/0000
---
+++ curso1/app.py
@@ -0,0 +1,9 @@
+print("Sabor Express\b")
+
+print("1. Cadastrar restaurente")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("4. Sair\n")
+
+opcao_escolhida = input("Escolha um opção: ")
+print("Você escolheu a opção", opcao_escolhida)
