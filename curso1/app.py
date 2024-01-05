c98a2192e3698b89b1767cf0edc7e516d89d8fac
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-04 21:26:01 -0300

    mm

--- curso1/app.py
+++ curso1/app.py
@@ -1,9 +1,80 @@
-print("Sabor Express\b")
+import os
 
-print("1. Cadastrar restaurente")
-print("2. Listar restaurante")
-print("3. Ativar restaurante")
-print("4. Sair\n")
+restaurantes = [
+    'Pizza'
+]
 
-opcao_escolhida = input("Escolha um opção: ")
-print("Você escolheu a opção", opcao_escolhida)
+
+def exibir_nome_programa():
+    print("""
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    """)
+
+def voltar_ao_menu_principal():
+    input('\nDigite um tecla para voltar para o menu ')
+    main()
+
+def exibir_subtitulo(texto):
+    os.system('clear')
+    print(texto)
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurente')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def opcao_invalida():
+    print('Opção inálida!\n')
+    voltar_ao_menu_principal()
+
+
+
+def cadastro_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes:\n')
+    nome_do_restaurante = input('Digite o nome do resutaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurante:\n')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha um opção: '))
+        if opcao_escolhida == 1:
+            cadastro_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Listar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def finalizar_app():
+    exibir_subtitulo('Finalizando o programa\n')
+
+
+def main():
+    os.system('clear')
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
