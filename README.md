016cc92a5475d81bbdb730d18de793be3768a373
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-06 16:11:51 -0300

    Fim do curso 2 (06/01/2024)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Formacao Alura Python OO
 
-1. [Curso de Python: crie a sua primeira aplicação](https://cursos.alura.com.br/course/python-crie-sua-primeira-aplicacao): Inicio 27/12/2023 - Fim 00/00/0000
+1. [Curso de Python: crie a sua primeira aplicação](https://cursos.alura.com.br/course/python-crie-sua-primeira-aplicacao): Inicio 27/12/2023 - Fim 05/01/2024
+2. [Curso de Python: aplicando a Orientação a Objetos](https://cursos.alura.com.br/course/python-aplicando-orientacao-objetos): Inicio: 05/01/2024 - 06/01/2024
--- curso1/app.py
+++ curso1/app.py
@@ -1,7 +1,9 @@
 import os
 
 restaurantes = [
-    'Pizza'
+    {'nome': 'Praça', 'categoria': 'Japonesa', 'ativo': False},
+    {'nome': 'Pizza Sumprea', 'categoria': 'Pizza', 'ativo': True},
+    {'nome': 'Cantina', 'categoria': 'Italiana', 'ativo': False},
 ]
 
 
@@ -21,12 +23,16 @@ def voltar_ao_menu_principal():
 
 def exibir_subtitulo(texto):
     os.system('clear')
+    linha = '*' * len(texto)
+    print(linha)
     print(texto)
+    print(linha)
+    print()
 
 def exibir_opcoes():
     print('1. Cadastrar restaurente')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alterar estado do restaurante')
     print('4. Sair\n')
 
 def opcao_invalida():
@@ -36,19 +42,55 @@ def opcao_invalida():
 
 
 def cadastro_novo_restaurante():
-    exibir_subtitulo('Cadastro de novos restaurantes:\n')
-    nome_do_restaurante = input('Digite o nome do resutaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    '''Essa função é responsável por cadadstrar u novo restaurante'''
+
+    exibir_subtitulo('Cadastro de novos restaurantes:')
+
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+
+    categoria = input(f'Digite o nome do categoria do restaurante {nome_do_restaurante}: ')
+
+    dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria': categoria, 'ativo': False}
+
+    restaurantes.append(dados_do_restaurante)
+
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
-    exibir_subtitulo('Listando os restaurante:\n')
+    exibir_subtitulo('Listando os restaurante:')
 
+    print(f"{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status")
     for restaurante in restaurantes:
-        print(f'.{restaurante}')
+        nome_restaurente = restaurante['nome']
+        categoria = restaurante['categoria']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurente.ljust(20)} | {categoria.ljust(20)} | {ativo}')
 
     voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alterando estado do restaurante')
+
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterarr o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = (
+                f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo']
+                else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            )
+            print(mensagem)
+
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
+
 
 def escolher_opcao():
     try:
@@ -58,16 +100,19 @@ def escolher_opcao():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print('Listar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
             opcao_invalida()
+    except KeyboardInterrupt:
+        finalizar_app()
     except:
         opcao_invalida()
 
+
 def finalizar_app():
-    exibir_subtitulo('Finalizando o programa\n')
+    exibir_subtitulo('Finalizando o programa')
 
 
 def main():
